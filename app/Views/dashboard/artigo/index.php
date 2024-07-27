4531c8385e40b706d8f7546dd43321d20600ae5e
Raul Diamantino <rauldiamantino25@gmail.com>
2024-07-27 00:50:54 -0300

    Feat: Adiciona coluna ações

--- app/Views/dashboard/artigo/index.php
+++ app/Views/dashboard/artigo/index.php
@@ -11,7 +11,7 @@
           <th scope="col" class="px-6 py-3 w-56">Título</th>
           <th scope="col" class="px-6 py-3 w-32">Categoria</th>
           <th scope="col" class="px-6 py-3 w-32 min-w-[120px]">Criado</th>
-          <th scope="col" class="px-6 py-3 w-32 min-w-[120px]">Modificado</th>
+          <th scope="col" class="px-6 py-3 w-32 min-w-[120px]">Ações</th>
         </tr>
       </thead>
       <tbody>
@@ -36,10 +36,22 @@
               <?php if ($subChave == 'Artigo.criado') { ?>
                 <td class="whitespace-normal p-6 w-32 min-w-[120px]"><?php echo $subLinha ?></td>
               <?php } ?>
-              <?php if ($subChave == 'Artigo.modificado') { ?>
-                <td class="whitespace-normal p-6 w-32 min-w-[120px]"><?php echo $subLinha ?></td>
-              <?php } ?>
             <?php endforeach; ?>
+            <td class="whitespace-normal p-6 w-32 min-w-[120px] flex gap-2">
+              <button type="button" class="flex gap-2 items-center justify-center py-2 px-3 bg-blue-800 text-white rounded-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                </svg>
+                Editar
+              </button>
+              <button type="button" class="flex gap-2 items-center justify-center py-2 px-3 bg-red-800 text-white rounded-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                </svg>
+                Apagar
+              </button>
+            </td>
           </tr>
         <?php endforeach; ?>
       </tbody>
