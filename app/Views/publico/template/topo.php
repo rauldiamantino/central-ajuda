c7f3c3ceb3f5e638a2da07003b9b5fe30317c09a
Raul Diamantino <rauldiamantino25@gmail.com>
2024-08-13 20:04:11 -0300

    Feat: Adiciona link para início no logo público

--- app/Views/publico/template/topo.php
+++ app/Views/publico/template/topo.php
@@ -1,6 +1,6 @@
 <header class="w-full pt-8 px-4">
   <div class="w-full flex justify-center items-center gap-4">
-    <img src="/img/logo-transparente.png" alt="" class="w-52">
+    <a href="/publico"><img src="/img/logo-transparente.png" alt="" class="w-52"></a>
     <h2 class="text-3xl tracking-tighter">central de ajuda</h2>
   </div>
 </header>
