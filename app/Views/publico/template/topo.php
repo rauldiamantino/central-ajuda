ce93b3f09cc3b0046e528e1cebae4ea0c7082e97
Raul Diamantino <rauldiamantino25@gmail.com>
2024-08-18 15:28:55 -0300

    WIP: Melhora campo de busca

--- app/Views/publico/template/topo.php
+++ app/Views/publico/template/topo.php
@@ -2,7 +2,7 @@
   <div class="w-8/12 flex gap-2 justify-between">
     <div class="w-full flex justify-start items-center gap-4">
       <a href="/publico"><img src="/img/logo-transparente.png" alt="" class="w-52"></a>
-      <h2 class="text-3xl tracking-tighter">central de ajuda</h2>
+      <h2 class="text-xl font-light tracking-tighter">central de ajuda</h2>
     </div>
     <div class="w-6/12 h-20 flex flex-col justify-center">
       <?php require_once 'busca.php' ?>
