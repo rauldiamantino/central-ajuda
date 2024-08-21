21668d8d9cf8d57d7ae7bb6257a5e26c5a5c5c19
Raul Diamantino <rauldiamantino25@gmail.com>
2024-08-20 23:59:20 -0300

    Fix: Apenas layout da tela de Empresa

--- app/Views/dashboard/empresa/formulario.php
+++ app/Views/dashboard/empresa/formulario.php
@@ -1,18 +1,18 @@
 <form method="POST" action="/empresa/<?php echo $empresa['Empresa.id'] ?>" class="border border-slate-200 w-full min-w-96 flex flex-col gap-4 p-4 rounded-lg shadow">
   <input type="hidden" name="_method" value="PUT">
   <div class="w-full flex flex-col gap-4">
-    <div class="flex gap-10">
-      <div class="w-full flex gap-4">
+    <div class="w-full flex justify-start gap-4">
+      <div class="w-max flex gap-4">
         <label class="flex flex-col items-start gap-1 cursor-pointer">
           <span class="block text-sm font-medium text-gray-700">Status</span>
           <input type="checkbox" value="<?php echo $empresa['Empresa.ativo']; ?>" class="sr-only peer" <?php echo $empresa['Empresa.ativo'] ? 'checked' : '' ?> disabled>
           <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-800"></div>
         </label>
       </div>
-    </div>
-    <div class="w-full">
-      <label for="empresa-editar-nome" class="block text-sm font-medium text-gray-700">Nome</label>
-      <input type="text" id="empresa-editar-nome" name="nome" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" value="<?php echo $empresa['Empresa.nome']; ?>">
+      <div class="w-full">
+        <label for="empresa-editar-nome" class="w-full block text-sm font-medium text-gray-700">Nome</label>
+        <input type="text" id="empresa-editar-nome" name="nome" class="mt-1 p-2 block w-full border border-gray-300 rounded-md" value="<?php echo $empresa['Empresa.nome']; ?>">
+      </div>
     </div>
     <div class="w-full">
       <label for="empresa-editar-cnpj" class="block text-sm font-medium text-gray-700">CNPJ</label>
