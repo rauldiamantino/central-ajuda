c2d0b017323b9c7e98f20371795a3ff3f6b876c7
Raul Diamantino <rauldiamantino25@gmail.com>
2024-08-21 14:32:51 -0300

    Feat: Inverte cor do topo na rolagem

--- app/Views/publico/template/topo.php
+++ app/Views/publico/template/topo.php
@@ -1,6 +1,6 @@
-<header class="sticky top-0 w-full flex justify-center py-8 px-4 bg-white transition-shadow duration-300 topo-publico">
+<header class="sticky top-0 w-full flex justify-center py-8 px-4 bg-slate-800 transition duration-300 topo-publico">
   <div class="w-8/12 flex gap-2 justify-between">
-    <div class="w-full flex justify-start items-center gap-4">
+    <div class="w-full flex justify-start items-center gap-4 invert inverter transition-invert duration-300">
       <a href="/p/<?php echo $subdominio ?>"><img src="/public/img/logo-transparente.png" alt="" class="w-52"></a>
       <h2 class="text-xl font-light tracking-tighter">central de ajuda</h2>
     </div>
--- public/js/publico/index.js
+++ public/js/publico/index.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   const topoPublico = document.querySelector('.topo-publico')
-
+  const topoPublicoInverter = document.querySelector('.inverter')
+console.log(topoPublicoInverter)
   function checarScroll() {
     const posicaoScroll = window.scrollY
 
     if (posicaoScroll > 50) {
       topoPublico.classList.add('shadow')
+      topoPublico.classList.remove('bg-slate-800')
+      topoPublico.classList.add('bg-white')
+      topoPublicoInverter.classList.remove('invert')
     } 
     else {
       topoPublico.classList.remove('shadow')
+      topoPublico.classList.remove('bg-white')
+      topoPublico.classList.add('bg-slate-800')
+      topoPublicoInverter.classList.add('invert')
     }
   }
 
