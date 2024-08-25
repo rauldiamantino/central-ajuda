4279b75c60da89bc24144c6104df886ea490e162
Raul Diamantino <rauldiamantino25@gmail.com>
2024-08-25 11:34:07 -0300

    Feat: Implementa menu lateral toggle

--- app/Views/dashboard/template/topo.php
+++ app/Views/dashboard/template/topo.php
@@ -1,7 +1,12 @@
 <header class="border-b border-slate-200 px-4 fixed top-0 w-screen bg-white z-20">
-  <nav class="bg-white">
+  <nav class="bg-white flex justify-between">
     <div class="p-4 flex items-center justify-between">
       <a href="/dashboard/artigos"><img src="/public/img/luminaOn.png" alt="luminaOn" class="w-40"></a>
     </div>
+    <button class="md:hidden btn-dashboard-menu-lateral">
+      <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="current">
+        <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/>
+      </svg>
+    </button>
   </nav>
 </header>
--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const btnNotificacaoErroFechar = document.querySelector('.js-dashboard-notificacao-erro-btn-fechar')
   const btnNotificacaoSucessoFechar = document.querySelector('.js-dashboard-notificacao-sucesso-btn-fechar')
   const btnNotificacaoNeutraFechar = document.querySelector('.js-dashboard-notificacao-neutra-btn-fechar')
+  const menuLateral = document.querySelector('.dashboard-menu-lateral')
+  const btnMenuLateral = document.querySelector('.btn-dashboard-menu-lateral')
 
   if (notificacaoErro) {
     setTimeout(() => fecharNotificacao(notificacaoErro), 5000)
@@ -42,10 +44,28 @@ document.addEventListener('DOMContentLoaded', function() {
       fecharNotificacao(notificacaoNeutra)
     })
   }
+
+  if (btnMenuLateral) {
+    btnMenuLateral.addEventListener('click', () => {
+      menuLateralToggle(menuLateral)
+    })
+  }
   
   removerAutocomplete()
   setTimeout(() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'}), 60)
 })
+
+const menuLateralToggle = (menuLateral) => {
+
+  if (menuLateral.classList.contains('-translate-x-full') && ! menuLateral.classList.contains('translate-x-0')) {
+    menuLateral.classList.add('translate-x-0')
+    menuLateral.classList.remove('-translate-x-full')
+  }
+  else if (! menuLateral.classList.contains('-translate-x-full') && menuLateral.classList.contains('translate-x-0')) {
+    menuLateral.classList.remove('translate-x-0')
+    menuLateral.classList.add('-translate-x-full')
+  }
+}
 
 const removerAutocomplete = () => {
   let inputs = document.querySelectorAll('input[autocomplete="off"]')
