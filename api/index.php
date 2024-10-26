5989802f7826db499b74767d7df45ea69ad5d156
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-26 01:01:44 -0300

    Feat: Index.php pasta API

--- api/index.php
+++ api/index.php
@@ -1,1 +1,14 @@
-<?php phpinfo() ?>
+<?php
+require '../vendor/autoload.php';
+
+use app\Core\Cache;
+use app\Core\SessaoUsuario;
+use app\Roteamento\Roteador;
+
+$sessaoUsuario = new SessaoUsuario();
+$roteador = new Roteador();
+
+$roteador->rotear();
+
+// Deixar comentado
+// Cache::resetarCache();
