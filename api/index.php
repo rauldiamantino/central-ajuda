5d77d7b3f598ec6877922401480840b684f4d420
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-26 01:10:44 -0300

    Feat: Index vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,2 @@
 <?php
-require '../vendor/autoload.php';
-
-use app\Core\Cache;
-use app\Core\SessaoUsuario;
-use app\Roteamento\Roteador;
-
-$sessaoUsuario = new SessaoUsuario();
-$roteador = new Roteador();
-
-$roteador->rotear();
-
-// Deixar comentado
-// Cache::resetarCache();
+echo 'olá mundo';
