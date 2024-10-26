fd27e0a80ad1cf75623ed8a74d9426bc79080b71
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-26 01:32:54 -0300

    Feat: Vercel - Atualiza index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
-echo 'olá mundo';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\Core\Cache;
+use app\Core\SessaoUsuario;
+use app\Roteamento\Roteador;
+
+$sessaoUsuario = new SessaoUsuario();
+$roteador = new Roteador();
+
+$roteador->rotear();
+
+// Deixar comentado
+// Cache::resetarCache();
