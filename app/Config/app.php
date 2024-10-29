33a6bbaf9522f7b2580da65588789d9ab0ea53df
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-28 22:12:09 -0300

    Fix: Banco em produção é Postgres

--- app/Config/app.php
+++ app/Config/app.php
@@ -8,5 +8,5 @@ define('SEGREDO_SESSAO', '!Central@Ajuda@2024@Dev@1');
 define('APLICACAO', $_SERVER['HTTP_HOST'] . 'central-ajuda');
 define('GRAVAR_CACHE', 0);
 define('MYSQL', 1);
-define('POSTGRESS', 2);
-define('SGBD', MYSQL);
+define('POSTGRES', 2);
+define('SGBD', POSTGRES);
