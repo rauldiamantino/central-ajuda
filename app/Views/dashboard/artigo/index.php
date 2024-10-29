8d425eb73ab869b29f56c8022b88cc29bdde082b
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-29 20:46:18 -0300

    Fix: Largura menu Artigos

--- app/Views/dashboard/artigo/index.php
+++ app/Views/dashboard/artigo/index.php
@@ -2,14 +2,14 @@
   <div class="relative p-4 w-full min-h-full flex flex-col">
     <div class="pb-4 w-full flex flex-col sm:flex-row justify-between items-start gap-10 sm:items-center">
       <h2 class="text-2xl font-semibold flex gap-2">Artigos</h2>
-      <div class="w-max flex gap-6 items-center rounded-md">
-        <button type="button" class="w-full flex gap-2 items-center justify-center text-black text-sm text-sm hover:underline" onclick="filtrarArtigos()">
+      <div class="w-full flex md:gap-6 justify-between md:justify-end items-center rounded-md">
+        <button type="button" class="w-max flex gap-2 items-center justify-center text-black text-sm text-sm hover:underline" onclick="filtrarArtigos()">
           <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
             <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
           </svg>
           Filtrar
         </button>
-        <button type="button" class="w-full flex gap-2 items-center justify-end text-black text-sm text-sm hover:underline" <?php echo count($artigos) > 1 ? 'onclick="buscarArtigos()"' : '' ?>>
+        <button type="button" class="w-max flex gap-2 items-center justify-end text-black text-sm text-sm hover:underline" <?php echo count($artigos) > 1 ? 'onclick="buscarArtigos()"' : '' ?>>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5m-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5"/>
           </svg>
