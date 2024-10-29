35d0fa1d9229d5da5c15ef8c75350b705ddb09a1
Raul Diamantino <rauldiamantino25@gmail.com>
2024-10-28 22:30:23 -0300

    Fix: URL para Central de Ajuda

--- app/Views/dashboard/template/topo.php
+++ app/Views/dashboard/template/topo.php
@@ -34,7 +34,7 @@
         </button>
       </li>
       <li class="px-8 py-5">
-        <button type="button" onclick="window.open('<?php echo subdominioDominio($this->usuarioLogado['subdominio']); ?>')" class="flex gap-3 items-center hover:text-gray-950">
+        <button type="button" onclick="window.open('/<?php echo $this->usuarioLogado['subdominio']; ?>')" class="flex gap-3 items-center hover:text-gray-950">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16" stroke="currentColor" stroke-width="0.3">
           <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
           <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
