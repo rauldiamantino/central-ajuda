b5205493409390d86737a493b9818924ab6429a9
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-02 02:29:15 -0300

    Fix: Padding Debug

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -70,7 +70,7 @@
     <div class="<?php echo $classeMargem ?>my-10 px-4 lg:px-14 flex max-w-screen">
       <div class="w-full">
         <h2 class="mb-5 text-2xl font-semibold">Debug</h2>
-        <div class="border border-slate-300 w-full p-10 bg-gray-200 text-gray-900 text-xs shadow rounded-md">
+        <div class="border border-slate-300 w-full p-4 lg:p-10 bg-gray-200 text-gray-900 text-xs shadow rounded-md">
           <div class="py-4 overflow-x-auto">
             <?php pr($this->sessaoUsuario->buscar('debug')); ?>
           </div>
