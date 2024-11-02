45fbb0d9f9696c6fcea485917424ac244ac3b282
root <rauldiamantino25@gmai.com>
2024-11-02 00:54:07 +0000

    Feat: Pasta Logs na Raíz

---
+++ app/Config/app.php
@@ -0,0 +1,12 @@
+<?php
+define('SEGREDO_SESSAO', '!Central@Ajuda@2024@Dev@1');
+define('APLICACAO', $_SERVER['HTTP_HOST'] . 'central-ajuda');
+define('GRAVAR_CACHE', 1);
+
+define('MYSQL_HOST', '137.184.202.254');
+define('MYSQL_NOME', 'central-ajuda');
+define('MYSQL_USUARIO', 'raul');
+define('MYSQL_SENHA', 'YQ1L=!nH~x7c.CQ..rDn');
+
+define('MEMCACHED_HOST', 'https://memcached-h2g2.onrender.com');
+define('MEMCACHED_PORT', 10000);
