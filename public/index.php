d654d1e55d89f0fd979b7efd350bd461b947abd3
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-02 01:45:24 -0300

    Fix: Debug

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require '../vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use app\Core\Cache;
 use app\Core\SessaoUsuario;
 use app\Roteamento\Roteador;
