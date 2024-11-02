4124830a390ef5ca3d5b7afc51c522bc29c8faf1
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-02 01:47:31 -0300

    Fix: Remove debug

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 require '../vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use app\Core\Cache;
 use app\Core\SessaoUsuario;
 use app\Roteamento\Roteador;
