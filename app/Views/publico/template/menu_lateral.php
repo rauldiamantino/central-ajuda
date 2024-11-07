dbc18ebcdd8a839876ab84bd5c0cae15d6050eca
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-07 08:25:43 -0300

    Fix: Reduz altura entre linhas do menu

--- app/Views/publico/template/menu_lateral.php
+++ app/Views/publico/template/menu_lateral.php
@@ -6,14 +6,14 @@
     <button class="absolute right-0 pt-10 px-8 h-max w-max md:hidden btn-publico-menu-lateral-fechar">
       <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="currentColor"><path d="m287-446.67 240 240L480-160 160-480l320-320 47 46.67-240 240h513v66.66H287Z"/></svg>
     </button>
-    <ul class="h-full py-10 border-r border-slate-200 px-4 flex flex-col gap-2">
+    <ul class="h-full py-10 border-r border-slate-200 px-4 flex flex-col">
 
       <?php if (isset($categorias[0]) and is_array($categorias[0])) { ?>
         <h3 class="w-full text-start mb-2 px-6 py-4 text-lg text-gray-600 font-light text-xs">CATEGORIAS</h3>
 
         <?php foreach ($categorias as $chave => $linha) : ?>
           <li class="w-full flex <?php echo $categoriaAtual == $linha['Categoria']['id'] ? ' font-bold' : '';?> hover:underline rounded-md">
-            <a href="<?php echo baseUrl('/' . $subdominio . '/categoria/' . $linha['Categoria']['id']); ?>" class="w-full flex gap-2 items-center px-6 py-4">
+            <a href="<?php echo baseUrl('/' . $subdominio . '/categoria/' . $linha['Categoria']['id']); ?>" class="w-full flex gap-2 items-center px-6 py-3">
               <?php echo $linha['Categoria']['nome'] ?>
 
               <?php if ($linha['Categoria']['ativo'] == INATIVO) { ?>
@@ -32,7 +32,7 @@
         <h3 class="w-full text-start mb-2 px-6 py-4 text-lg text-gray-600 font-light text-xs">ARTIGOS RELACIONADOS</h3>
         <?php foreach ($demaisArtigos as $chave => $linha) : ?>
           <li class="w-full flex <?php echo $artigoAtual == $linha['Artigo']['id'] ? ' font-bold' : '';?> hover:underline rounded-md">
-            <a href="<?php echo baseUrl('/' . $subdominio . '/artigo/' . $linha['Artigo']['id']); ?>" class="w-full flex items-center gap-2 px-6 py-4"><?php echo $linha['Artigo']['titulo'] ?>
+            <a href="<?php echo baseUrl('/' . $subdominio . '/artigo/' . $linha['Artigo']['id']); ?>" class="w-full flex items-center gap-2 px-6 py-3"><?php echo $linha['Artigo']['titulo'] ?>
               <?php if ($linha['Artigo']['ativo'] == INATIVO) { ?>
                 <div class="text-red-800">
                   <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 16 16">
