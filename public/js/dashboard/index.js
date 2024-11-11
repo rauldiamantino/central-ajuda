70ce18278e99d277c5e284a878f4684c7e7aaab3
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-11 02:20:39 -0300

    Fix: Rolagem vertical

--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -223,10 +223,10 @@ function fecharTextoModal(event) {
 
 function abrirModalAdicionar() {
   const modal = document.querySelector('.modal-conteudo-texto-adicionar')
-  modal.classList.remove('hidden')
 
   setTimeout(() => {
     modal.scrollIntoView({ behavior: 'smooth' })
+    modal.classList.remove('hidden')
   }, 100)
 }
 
