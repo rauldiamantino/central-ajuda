40e734fa7495899d57a9c21354f107e9371f43f9
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-15 01:23:01 -0300

    Fix: Remove debug

--- app/Views/dashboard/artigo/editar/conteudo/pre-visualizacao.php
+++ app/Views/dashboard/artigo/editar/conteudo/pre-visualizacao.php
@@ -1,4 +1,3 @@
-<?php pr($this) ?>
 <div class="w-full h-full bg-white p-5 lg:p-10 rounded-md pers-publico-artigo template-cor-<?php echo $this->usuarioLogado['corPrimaria']; ?> dashboard-pre-visualizacao">
   <div class="flex flex-col justify-between items-start gap-2 md:pt-5 px-3">
     <div class="px-3 flex gap-2 items-center">
