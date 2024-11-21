12930dc90c21b9863aab1e0039c99782abcf2d8e
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-21 08:58:36 -0300

    Feat: Link Ver na Central de Ajuda

--- app/Views/dashboard/categoria/editar/index.php
+++ app/Views/dashboard/categoria/editar/index.php
@@ -1,12 +1,8 @@
 <div class="relative w-full h-max flex flex-col">
   <div class="mb-4 w-full flex flex-col lg:flex-row justify-between items-start lg:items-center">
     <div class="mb-5 w-full h-full flex flex-col justify-end">
-      <h2 class="text-3xl font-semibold flex flex-wrap sm:gap-2">Editar categoria<span class="text-gray-400 font-light italic">
-        <a href="/<?php echo $this->usuarioLogado['subdominio']; ?>/categoria/<?php echo $categoria[0]['Categoria']['id'] ?>" target="_blank" class="text-sm text-gray-400 font-light italic hover:underline">
-          (Categoria #<?php echo $categoria[0]['Categoria']['id']; ?>)
-        </a>
-      </h2>
-      <p class="text-gray-600">Aqui estão todos os artigos da categoria <span class="font-semibold underline"><?php echo $categoria[0]['Categoria']['nome'] ?></span></p>
+      <h2 class="text-3xl font-semibold flex flex-wrap sm:gap-2"><?php echo $categoria[0]['Categoria']['nome'] ?></h2>
+      <p class="text-gray-600">Pronto! Aqui estão todos os artigos desta categoria.</p>
     </div>
     <div class="py-2 w-full h-full flex gap-2 items-start justify-end">
       <a href="<?php echo baseUrl('/' . $this->usuarioLogado['subdominio'] . '/dashboard/categorias'); ?>" class="<?php echo CLASSES_DASH_BUTTON_VOLTAR; ?>">Voltar</a>
@@ -22,6 +18,12 @@
           </svg>
         </button>
         <ul class="absolute top-12 right-0 md:-right-10 border border-slate-300 lg:mx-10 flex flex-col justify-center bg-white text-gray-600 rounded-md shadow hidden menu-auxiliar menu-auxiliar-categoria">
+          <li class="px-4 py-3">
+            <button type="button" onclick="window.open('/<?php echo $this->usuarioLogado['subdominio'] . '/categoria/' . $categoria[0]['Categoria']['id']; ?>')" class="flex gap-3 items-center hover:text-gray-950">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16"><path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/></svg>
+              <span class="whitespace-nowrap">Ver na central</span>
+            </button>
+          </li>
           <li class="px-4 py-3">
             <button type="button" onclick="document.querySelector('.menu-editar-categoria').showModal()" class="flex gap-3 items-center hover:text-gray-950 botao-abrir-menu-editar-categoria">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
