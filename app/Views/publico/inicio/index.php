375ab9251f1accf9f46f34473b64d5c8aefab60b
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-22 13:12:04 -0300

    Fix: Publico - Gap blocos de início

--- app/Views/publico/inicio/index.php
+++ app/Views/publico/inicio/index.php
@@ -1,7 +1,7 @@
 <div class="mx-auto w-full flex flex-col items-center gap-16 md:px-4 xl:px-6 py-12">
   <div class="w-full flex flex-col gap-2">
     <h3 class="font-extralight text-xs text-gray-800">CATEGORIAS</h3>
-    <div class="w-full h-max grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-start gap-2 xl:gap-4">
+    <div class="w-full h-max grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-start gap-4">
       <?php foreach ($categorias as $chave => $linha): ?>
         <a href="<?php echo baseUrl('/' . $subdominio . '/categoria/' . $linha['Categoria']['id']); ?>" class="border border-slate-200 shadow p-5 h-full min-h-[200px] bg-white flex flex-col gap-4 justify-start items-center text-center rounded-lg hover:scale-105 duration-100">
           <?php if ($linha['Categoria']['icone'] and file_get_contents($linha['Categoria']['icone'])) { ?>
