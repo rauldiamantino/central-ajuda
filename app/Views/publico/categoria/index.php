f79b6afa57a9c1af3cdc8cd9b9583115542dd237
Raul Diamantino <rauldiamantino25@gmail.com>
2024-11-26 00:39:49 -0300

    Fix: Z-index botão editar

--- app/Views/publico/categoria/index.php
+++ app/Views/publico/categoria/index.php
@@ -59,7 +59,7 @@
 </div>
 
 <?php if ($this->usuarioLogado['id'] and $this->usuarioLogado['subdominio']) { ?>
-  <div class="fixed bottom-32 right-4 sm:right-6 md:right-10 z-50 rounded-full hover:scale-110 duration-100 pers-publico-botao-editar template-cor-<?php echo $corPrimaria; ?>">
+  <div class="fixed bottom-32 right-4 sm:right-6 md:right-10 z-20 rounded-full hover:scale-110 duration-100 pers-publico-botao-editar template-cor-<?php echo $corPrimaria; ?>">
     <button type="button" onclick="window.open('/<?php echo $this->usuarioLogado['subdominio'] . '/dashboard/categoria/editar/' . $artigos[0]['Categoria']['id']; ?>')" class="flex items-center p-3 bg-black/15 rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-10 h-10">
         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
