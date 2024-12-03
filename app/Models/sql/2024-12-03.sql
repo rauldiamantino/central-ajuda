28c366115405923a9da64bbeb7d50c0a8f780ce1
Raul Diamantino <rauldiamantino25@gmail.com>
2024-12-03 14:00:57 -0300

    Fix: Delimitador de query

--- app/Models/sql/2024-12-03.sql
+++ app/Models/sql/2024-12-03.sql
@@ -1,14 +1,14 @@
 -- meta categorias
 ALTER TABLE `categorias`
 ADD COLUMN `meta_titulo` VARCHAR(255) DEFAULT '' AFTER `ordem`,
-ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`
+ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`;
 
 -- meta artigos
 ALTER TABLE `artigos`
 ADD COLUMN `meta_titulo` VARCHAR(255) DEFAULT '' AFTER `ordem`,
-ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`
+ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`;
 
 -- meta empresas
 ALTER TABLE `empresas`
 ADD COLUMN `meta_titulo` VARCHAR(255) DEFAULT '' AFTER `cor_primaria`,
-ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`
+ADD COLUMN `meta_descricao` VARCHAR(255) DEFAULT '' AFTER `meta_titulo`;
