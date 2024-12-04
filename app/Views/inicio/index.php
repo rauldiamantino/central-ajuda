5c3d588f391c41b92811b80a8a8e74fefac6e718
Raul Diamantino <rauldiamantino25@gmail.com>
2024-12-03 23:01:55 -0300

    Feat: Adiciona meta tags nos recursos

--- app/Views/inicio/index.php
+++ app/Views/inicio/index.php
@@ -417,6 +417,14 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                   </svg>
                 </span>
+                <span>Meta tags (SEO)</span>
+              </li>
+              <li class="font-light grid grid-cols-[auto,1fr] gap-2 items-center">
+                <span class="text-blue-600 w-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  </svg>
+                </span>
                 <span>Artigos públicos e privados</span>
               </li>
               <li class="font-light grid grid-cols-[auto,1fr] gap-2 items-center">
@@ -536,6 +544,14 @@
                   </svg>
                 </span>
                 <span>2 GB de armazenamento</span>
+              </li>
+              <li class="font-light grid grid-cols-[auto,1fr] gap-2 items-center">
+                <span class="text-blue-600 w-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  </svg>
+                </span>
+                <span>Meta tags (SEO)</span>
               </li>
               <li class="font-light grid grid-cols-[auto,1fr] gap-2 items-center">
                 <span class="text-blue-600 w-6">
