4f67e00f6088b28adf5434e12dc5b77e3c98baf0
Raul Diamantino <rauldiamantino25@gmail.com>
2024-12-05 14:01:16 -0300

    Fix: Melhora visual da paginação pública

--- app/Views/publico/busca/paginacao.php
+++ app/Views/publico/busca/paginacao.php
@@ -1,8 +1,8 @@
 <?php if ($paginasTotal > 1) { ?>
   <?php // Paginação ?>
-  <div class="pt-20 sticky inset-x-0 w-full h-max py-2 flex justify-start items-center gap-1 bg-white">
+  <div class="mt-10 sticky inset-x-0 w-full h-max py-2 flex justify-start items-center gap-1 rounded-md">
     <?php if ($pagina > 1) { ?>
-      <a href="<?php echo baseUrl('/' . $subdominio . '/buscar?texto_busca=' . $textoBusca . '&pagina=' . $pagina - 1); ?>" class="border border-slate-100 bg-slate-100 hover:bg-slate-200 text-gray-500 hover:text-black p-3 rounded-lg">
+      <a href="<?php echo baseUrl('/' . $subdominio . '/buscar?texto_busca=' . $textoBusca . '&pagina=' . $pagina - 1); ?>" class="border border-gray-200 bg-gray-50 hover:bg-gray-200 text-gray-500 hover:text-black p-3 rounded-lg">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" stroke="currentColor" stroke-width="2" />
         </svg>
@@ -10,7 +10,7 @@
     <?php } ?>
 
     <?php if ($pagina <= 1) { ?>
-      <div class="border border-slate-100 text-gray-500 p-3 rounded-lg">
+      <div class="border border-gray-200 text-gray-500 p-3 rounded-lg">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" stroke="currentColor" stroke-width="2" />
         </svg>
@@ -19,11 +19,11 @@
 
     <form action="<?php echo baseUrl('/' . $subdominio . '/buscar'); ?>" method="get">
       <input type="hidden" name="texto_busca" value="<?php echo $textoBusca ?>">
-      <input type="number" name="pagina" value="<?php echo $pagina ?>" class="p-2 w-16 border-2 border-slate-100 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-lg artigo-numero-pagina">
+      <input type="number" name="pagina" value="<?php echo $pagina ?>" class="p-2 w-16 border-2 border-gray-200 text-center [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-lg artigo-numero-pagina">
     </form>
 
     <?php if ($pagina < $paginasTotal) { ?>
-      <a href="<?php echo baseUrl('/' . $subdominio . '/buscar?texto_busca=' . $textoBusca . '&pagina=' . $pagina + 1); ?>" class="border border-slate-100 bg-slate-100 hover:bg-slate-200 text-gray-500 hover:text-black p-3 rounded-lg">
+      <a href="<?php echo baseUrl('/' . $subdominio . '/buscar?texto_busca=' . $textoBusca . '&pagina=' . $pagina + 1); ?>" class="border border-gray-200 bg-gray-50 hover:bg-gray-200 text-gray-500 hover:text-black p-3 rounded-lg">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" stroke="currentColor" stroke-width="2" />
         </svg>
@@ -31,7 +31,7 @@
     <?php } ?>
 
     <?php if ($pagina == $paginasTotal) { ?>
-      <div class="border border-slate-100 text-gray-500 p-3 rounded-lg">
+      <div class="border border-gray-200 text-gray-500 p-3 rounded-lg">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" stroke="currentColor" stroke-width="2" />
         </svg>
