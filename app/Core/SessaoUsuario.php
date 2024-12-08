9d5f18bde397f60a65fb047d93fe6d610492de64
Raul Diamantino <rauldiamantino25@gmail.com>
2024-12-08 03:14:54 -0300

    Fix: Grava sessão apenas se o valor for diferente

--- app/Core/SessaoUsuario.php
+++ app/Core/SessaoUsuario.php
@@ -41,10 +41,22 @@ class SessaoUsuario
 
   public function definir($chave, $valor, $array = false)
   {
+    $valorAtual = $this->buscar($chave);
+
     if ($array) {
-      $_SESSION[ $chave ][] = $valor;
+
+      if (! is_array($valorAtual)) {
+        $_SESSION[ $chave ] = [];
+      }
+
+      if (! in_array($valor, $_SESSION[$chave], true)) {
+        $_SESSION[ $chave ][] = $valor;
+      }
+
+      return;
     }
-    else {
+
+    if ($valorAtual !== $valor) {
       $_SESSION[ $chave ] = $valor;
     }
   }
