5b38e038a0cf389dba703dfe45a6bfd975971dfa
Raul Diamantino <rauldiamantino25@gmail.com>
2025-02-16 17:06:12 -0300

    Fix: Alinhamento do texto do campo de busca

--- app/Views/publico/inicio/formulario-busca.php
+++ app/Views/publico/inicio/formulario-busca.php
@@ -1,8 +1,8 @@
 <div class="w-full <?php echo $classesLarguraGeral ?> flex <?php echo $classesBuscaAlinhamento . ' ' . $classesBuscaAlinhamentoTexto; ?>">
   <div class="w-full <?php echo $classesBuscaTamanho; ?> flex flex-col items-start gap-6">
-    <div class="flex flex-col gap-3">
-      <h2 class="font-bold text-3xl"><?php echo Helper::ajuste('publico_inicio_titulo'); ?></h2>
-      <div class="font-light"><?php echo Helper::ajuste('publico_inicio_subtitulo'); ?></div>
+    <div class="w-full flex flex-col gap-3">
+      <h2 class="w-full font-bold text-3xl"><?php echo Helper::ajuste('publico_inicio_titulo'); ?></h2>
+      <div class="w-full block font-light"><?php echo Helper::ajuste('publico_inicio_subtitulo'); ?></div>
     </div>
     <div class="w-full flex flex-col justify-center">
 
