b9b8f19b1cc67ea587d33f4cf47c9b00cf30dba2
Raul Diamantino <rauldiamantino25@gmail.com>
2025-02-18 01:00:25 -0300

    Fix: Preload do logo

--- app/Views/publico/template/topo.php
+++ app/Views/publico/template/topo.php
@@ -30,7 +30,7 @@ if ((int) Helper::ajuste('publico_topo_borda_demais') == INATIVO and ! isset($in
 ?>
 
 <?php // Preloads ?>
-<link rel="preload" href="<?php echo $this->renderImagem($logo); ?>" as="image">
+<link rel="preload" href="<?php echo $this->renderImagem($logo); ?>" as="image" type="image/png">
 
 <header class="border-b <?php echo $classesTopoBorda . ' ' . $classesTopoFixo. ' ' . $classesTopoCor ?> z-20 top-0 w-full h-24 lg:mx-auto flex justify-center items-center <?php echo isset($inicio) ? 'py-12' : 'py-20'; ?> md:py-4 px-4 xl:px-0 md:gap-10 transition duration-300 topo-publico">
 
